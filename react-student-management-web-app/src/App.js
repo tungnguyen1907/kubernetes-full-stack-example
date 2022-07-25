2d026766480d7aa972988f73301f344c493dccde
Your Name <you@example.com>
2022-07-25 04:39:24 +0000

    change

--- react-student-management-web-app/src/App.js
+++ react-student-management-web-app/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
           <a href="/students" className="navbar-brand">
-            Student Management APP 
+            Student Management APP(VietNamTeam) 
           </a>
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
